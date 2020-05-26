efae69a65512696aa1a0581170e7c73452caaacf
prabjyot29 <prabjyotsudan@eniak.xyz>
2020-05-26 23:11:20 +0530

    travis changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts232
+          Learn Reacts2321
         </a>
       </header>
     </div>
