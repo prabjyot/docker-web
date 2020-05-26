e583c4f1a0658da338dd3a0d762f029222461dca
prabjyot29 <prabjyotsudan@eniak.xyz>
2020-05-26 21:57:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacts232
         </a>
       </header>
     </div>
